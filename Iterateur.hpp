7e79a5630a9e6094d8b9d9452ffe7fa45ee854b9
Hamza Khachan <52715910+hamzakh1@users.noreply.github.com>
2021-11-16 18:28:40 -0500

    Add files via upload

---
+++ Iterateur.hpp
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "noeud.hpp"
+#include "gsl/gsl_assert"
+template<typename T>
+class Iterateur
+{
+public:
+	//TODO: Constructeur(s).
+	Iterateur(Noeud* position = Noeud::past_end) : position_(position) {};
+	void avancer()
+	{
+		Expects(position_ != nullptr);
+		//TODO: changez la position de l'itérateur pour le noeud suivant
+		position_ = position_->next_;
+	}
+	void reculer()
+	{
+		//NOTE: On ne demande pas de supporter de reculer à partir de l'itérateur end().
+		Expects(position_ != nullptr);
+		//TODO: Changez la position de l'itérateur pour le noeud précédent
+		position_ = position_->previous_;
+	}
+	T& operator*()
+	{
+		Expects(position_ != Noeud::past_end); //unsure
+		return position_->donnee_;
+	}
+	Iterateur& operator++()
+	{
+		Expects(position_ != Noeud::past_end); //unsure
+		position_ = position_->next_;
+
+		return *this;
+	}
+	Iterateur& operator--()
+	{
+
+	}
+	//TODO: Ajouter ce qu'il manque pour que les boucles sur intervalles fonctionnent sur une ListeLiee.
+	bool operator==(const Iterateur<T>& it) const = default;
+private:
+	Noeud<T>* position_;
+	friend class ListeLiee<T>;
+};
---
+++ Noeud.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <gsl/pointers>
+template<typename T> class ListeLiee;
+template<typename T> class Iterateur;
+template<typename T>
+class Noeud
+{
+public:
+	//TODO: Constructeur(s).
+	Noeud(const T& data) : data_(data) {};
+private:
+	//TODO: Attributs d'un noeud
+	Noeud<T>* next_ = past_end; // ptr par defaut
+	Noeud<T>* previous_ = past_end; // ptr par defaut
+	T donnee_;
+	inline static constexpr Noeud* past_end = nullptr;
+	friend class ListeLiee<T>;
+	friend class Iterateur<T>;
+};
